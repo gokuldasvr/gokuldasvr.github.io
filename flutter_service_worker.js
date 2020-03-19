edf851dbb6dd85f8f356b436da08bbaf801c34c2
Gokuldas V R <gokul.vr@hapiens.com>
2020-03-20 01:03:49 +0530

    publish 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,19 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "9cb336b304e97a85033ad00f7dff6512",
-"/main.dart.js": "663be839b96948f505a6adf62447b55f",
-"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/manifest.json": "9b2c6709bd2845ee1054a4d37f427087",
-"/assets/LICENSE": "305a2fac8def79acdd1f039fe8646f3d",
-"/assets/AssetManifest.json": "d3995406e6bc75c11b5d4216917c44d3",
-"/assets/FontManifest.json": "bfe53b2ad00884e9b5fb3187b6a76654",
+  "/index.html": "071e42b1237acbc0514fc3a1a40ecca8",
+"/favicon.jpeg": "0ed895bddc2f8971109cf5b087e09e35",
+"/main.dart.js": "33369845b0485cf8adc12e71a82bb613",
+"/icons/Icon-512.jpeg": "df76f0ba7c567008bf5de6ebef9d5bb2",
+"/icons/Icon-192.jpeg": "0ed895bddc2f8971109cf5b087e09e35",
+"/manifest.json": "56aae994371aa0946f5b763c287a9dfd",
+"/assets/LICENSE": "5f7fa55feef3338a64e944ebfeb3e9a9",
+"/assets/AssetManifest.json": "4311f4484335584946f1cf21c8866539",
+"/assets/FontManifest.json": "514cf0c2611612726c1ab0c612c4d650",
 "/assets/fonts/dancing_script/DancingScript-Bold.ttf": "76c145c2f3f1c17fd11ec9a3740521f6",
 "/assets/fonts/dancing_script/DancingScript-Regular.ttf": "c4434ab21f7144bbcf88c9a35ae3f075",
+"/assets/fonts/exo/Exo-Regular.ttf": "8bee4f80d69101a696aea9b436835337",
+"/assets/fonts/exo/Exo-Light.ttf": "300557d530b5b680be682bb0f5611183",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
